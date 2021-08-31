d8b67cbd3153a8bced52a85aafc6e0ba817ba015
yun wang <512458266@qq.com>
2021-09-01 00:51:19 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ times=`grep -Po 'push_times\-(\d+)\-'  ./docs/index.md |grep -Po '(\d+)' `
 times=`expr $times + 1`
 
 label="![CI](https:\/\/github.com\/yunwan1x\/my-document\/actions\/workflows\/documentation.yml\/badge.svg) ![buidtime](https:\/\/img.shields.io\/badge\/build_time-$time-519dd9.svg) ![push_times](https:\/\/img.shields.io\/badge\/push_times-$times-orange.svg)"
-sed -i "s/.*\[buidtime\].*)/$label/g" ./docs/index.md
+sed -i "s/.*!\[CI\](https:\/\/github\.com.*/$label/g" ./docs/index.md
 
 
 git add ./docs/index.md && git commit -m "update by github action" && git push
--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ times=`expr $times + 1`
 
 
 label="![CI](https:\/\/github.com\/yunwan1x\/my-document\/actions\/workflows\/documentation.yml\/badge.svg) ![buidtime](https:\/\/img.shields.io\/badge\/build_time-$time-519dd9.svg) ![push_times](https:\/\/img.shields.io\/badge\/push_times-$times-orange.svg)"
-sed -i "s/.*\[buidtime\].*)/$label/g" ./docs/index.md
+sed -i "s/.*!\[CI\](https:\/\/github\.com.*/$label/g" ./docs/index.md
 
